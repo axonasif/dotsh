f2bfc36817fedf6828444fded0bcbecacaa2df00
AXON <axonasif@gmail.com>
2022-02-17 13:37:56 +0000

    feat: Support merging private dotfiles

--- install.sh
+++ install.sh
@@ -5,4 +5,19 @@ if test ! -e /ide/bin/gitpod-code || test ! -v GITPOD_REPO_ROOT; then {
 } fi
 
 _source_dir="$(readlink -f "$0")" && _source_dir="${_source_dir%/*}";
-echo " Just testing"
+_private_dir="$_source_dir/.private";
+
+# Fetch private stuff
+git clone https://github.com/axonasif/dotfiles.private "$_private_dir" && {
+    mapfile -t _private_files < <(find "$_private_dir");
+    for _file in "${_private_files[@]}"; do {
+        _target_file="$HOME/${_file###${_private_dir}/}";
+        _target_dir="${_target_file%/*}";
+        if test ! -d "$_target_dir"; then {
+            mkdir -p "$_target_dir";
+        } fi
+        ln -srf "$_file" "$_target_file";
+    }  done
+}
+
+printf "Just testing\n"
