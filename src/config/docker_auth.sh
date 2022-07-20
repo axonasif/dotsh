1c71d849034a3222d549782e59e7b419cd8131b9
AXON <axonasif@gmail.com>
2022-07-20 17:19:08 +0000

    fix: typo

--- src/config/docker_auth.sh
+++ src/config/docker_auth.sh
@@ -4,7 +4,7 @@ function docker_auth() {
     if test -v $var_name; then {
         log::info "Setting up docker login credentials";
         mkdir -p "${target%/*}";
-        println '{"auths":{"https://index.docker.io/v1/":{"auth":"%s"}}}\n' "${!var_name}" > "$target";
+        printf '{"auths":{"https://index.docker.io/v1/":{"auth":"%s"}}}\n' "${!var_name}" > "$target";
     } else {
         log::warn "$var_name is not set";
     } fi
