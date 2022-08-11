ddbe467244d548d66cac5d538deed30b54a623fd
AXON <axonasif@gmail.com>
2022-08-11 07:32:19 +0000

    up

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-main@bashbox%10902 () 
+main@bashbox%335 () 
 { 
     function process::self::exit () 
     { 
@@ -50,7 +50,7 @@ main@bashbox%10902 ()
     trap 'BB_ERR_MSG="UNCAUGHT EXCEPTION" log::error "$BASH_COMMAND" || process::self::exit' ERR;
     ___self="$0";
     ___self_PID="$$";
-    ___MAIN_FUNCNAME="main@bashbox%10902";
+    ___MAIN_FUNCNAME="main@bashbox%335";
     ___self_NAME="dotfiles";
     ___self_CODENAME="dotfiles";
     ___self_AUTHORS=("AXON <axonasif@gmail.com>");
@@ -272,7 +272,7 @@ main@bashbox%10902 ()
                             };
                         fi
                     };
-                    if [ "$BASH" == /bin/bash ] || [ "$PPID" == "$(pgrep -f "supervisor run" | head -n1)" ]; then
+                    if [ "$BASH" == /bin/bash ] || [ "$PPID" == "$(pgrep -f "supervisor run" | head -n1)" ] || ! read -t 2 -r external_commands; then
                         { 
                             termout=/tmp/.termout.$$;
                             if test ! -v bash_ran_once; then
@@ -390,4 +390,4 @@ EOF
     wait;
     exit
 }
-main@bashbox%10902 "$@";
+main@bashbox%335 "$@";
