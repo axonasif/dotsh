7be35f6696b8c9ccb71954c28ba56e5b45f46150
AXON <axonasif@gmail.com>
2022-08-21 15:04:18 +0000

    test: feat532

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-main@bashbox%11533 () 
+main@bashbox%14025 () 
 { 
     function process::self::exit () 
     { 
@@ -50,7 +50,7 @@ main@bashbox%11533 ()
     trap 'BB_ERR_MSG="UNCAUGHT EXCEPTION" log::error "$BASH_COMMAND" || process::self::exit' ERR;
     ___self="$0";
     ___self_PID="$$";
-    ___MAIN_FUNCNAME="main@bashbox%11533";
+    ___MAIN_FUNCNAME="main@bashbox%14025";
     ___self_NAME="dotfiles";
     ___self_CODENAME="dotfiles";
     ___self_AUTHORS=("AXON <axonasif@gmail.com>");
@@ -320,8 +320,6 @@ main@bashbox%11533 ()
                 log::info "Setting tmux as the interactive shell for Gitpod task terminals";
                 function inject_tmux () 
                 { 
-                    ls /usr/bin/tmux;
-                    trap 'read -p "$BASH_LINENO;waiting..." && $BASH --norc' EXIT ERR SIGTERM SIGINT;
                     function create_session () 
                     { 
                         tmux new-session -n home -ds main 2> /dev/null || :;
@@ -483,4 +481,4 @@ JSON
     wait;
     exit
 }
-main@bashbox%11533 "$@";
+main@bashbox%14025 "$@";
