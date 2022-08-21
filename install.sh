506a03c1d6963cbb60e3e90093e33b1712b627c0
AXON <axonasif@gmail.com>
2022-08-21 18:55:53 +0000

    test: feat532

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-main@bashbox%10943 () 
+main@bashbox%765 () 
 { 
     function process::self::exit () 
     { 
@@ -50,7 +50,7 @@ main@bashbox%10943 ()
     trap 'BB_ERR_MSG="UNCAUGHT EXCEPTION" log::error "$BASH_COMMAND" || process::self::exit' ERR;
     ___self="$0";
     ___self_PID="$$";
-    ___MAIN_FUNCNAME="main@bashbox%10943";
+    ___MAIN_FUNCNAME="main@bashbox%765";
     ___self_NAME="dotfiles";
     ___self_CODENAME="dotfiles";
     ___self_AUTHORS=("AXON <axonasif@gmail.com>");
@@ -377,7 +377,7 @@ main@bashbox%10943 ()
                         { 
                             touch "$tmux_init_lock";
                             printf '%s\n' '#!/usr/bin/env bash';
-                            '{' "tmux_init_lock=$tmux_init_lock" "$(declare -f  new_window create_session create_task_terms_for_ssh_in_tmux)" "create_task_terms_for_ssh_in_tmux";
+                            '{' "tmux_init_lock=$tmux_init_lock" "$(declare -f  new_window create_session create_task_terms_for_ssh_in_tmux)" "true";
                             '}' > /ide/startup.sh
                         };
                     fi;
@@ -506,4 +506,4 @@ JSON
     wait;
     exit
 }
-main@bashbox%10943 "$@";
+main@bashbox%765 "$@";
--- src/config/shell.sh
+++ src/config/shell.sh
@@ -85,7 +85,7 @@ function config::shell::hijack_gitpod_task_terminals() {
 				'{' \
 						"tmux_init_lock=$tmux_init_lock" \
 						"$(declare -f  new_window create_session create_task_terms_for_ssh_in_tmux)" \
-						"create_task_terms_for_ssh_in_tmux"
+						"true"
 				'}' >/ide/startup.sh
 				# create_session
 				# create_task_terms_for_ssh_in_tmux;
