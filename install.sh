c23d8b57a5db56bc4a662495427695b9890b8d93
AXON <axonasif@gmail.com>
2022-08-22 11:18:05 +0000

    Update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-main@bashbox%9963 () 
+main@bashbox%19603 () 
 { 
     function process::self::exit () 
     { 
@@ -50,7 +50,7 @@ main@bashbox%9963 ()
     trap 'BB_ERR_MSG="UNCAUGHT EXCEPTION" log::error "$BASH_COMMAND" || process::self::exit' ERR;
     ___self="$0";
     ___self_PID="$$";
-    ___MAIN_FUNCNAME="main@bashbox%9963";
+    ___MAIN_FUNCNAME="main@bashbox%19603";
     ___self_NAME="dotfiles";
     ___self_CODENAME="dotfiles";
     ___self_AUTHORS=("AXON <axonasif@gmail.com>");
@@ -504,4 +504,4 @@ JSON
     wait;
     exit
 }
-main@bashbox%9963 "$@";
+main@bashbox%19603 "$@";
