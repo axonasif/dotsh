8baf9386098cc9167fe7315a82441a1be46f5214
AXON <axonasif@gmail.com>
2022-08-22 11:57:12 +0000

    Update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-main@bashbox%10353 () 
+main@bashbox%1109 () 
 { 
     function process::self::exit () 
     { 
@@ -50,7 +50,7 @@ main@bashbox%10353 ()
     trap 'BB_ERR_MSG="UNCAUGHT EXCEPTION" log::error "$BASH_COMMAND" || process::self::exit' ERR;
     ___self="$0";
     ___self_PID="$$";
-    ___MAIN_FUNCNAME="main@bashbox%10353";
+    ___MAIN_FUNCNAME="main@bashbox%1109";
     ___self_NAME="dotfiles";
     ___self_CODENAME="dotfiles";
     ___self_AUTHORS=("AXON <axonasif@gmail.com>");
@@ -504,4 +504,4 @@ JSON
     wait;
     exit
 }
-main@bashbox%10353 "$@";
+main@bashbox%1109 "$@";
--- src/utils/vimpod.py
+++ src/utils/vimpod.py
@@ -16,7 +16,7 @@ CLUSTER_HOST = getenv('GITPOD_WORKSPACE_CLUSTER_HOST')
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         logfile.flush()
-        if "supervisor" in self.path:
+        if "supervisor" in self.path or "version" in self.path:
             self.send_response(200)
             # self.send_header('Content-type', 'text/html')
             # self.end_headers()
@@ -28,5 +28,5 @@ class handler(BaseHTTPRequestHandler):
             self.end_headers()
 
 
-with HTTPServer(('0.0.0.0', 23000), handler) as server:
+with HTTPServer(('0.0.0.0', 25000), handler) as server:
     server.serve_forever()
