cb076ada916063253ce3203956b36045c7285324
AXON <axonasif@gmail.com>
2022-08-24 09:27:09 +0000

    up

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-main@bashbox%24850 () 
+main@bashbox%15158 () 
 { 
     function process::self::exit () 
     { 
@@ -50,7 +50,7 @@ main@bashbox%24850 ()
     trap 'BB_ERR_MSG="UNCAUGHT EXCEPTION" log::error "$BASH_COMMAND" || process::self::exit' ERR;
     ___self="$0";
     ___self_PID="$$";
-    ___MAIN_FUNCNAME="main@bashbox%24850";
+    ___MAIN_FUNCNAME="main@bashbox%15158";
     ___self_NAME="dotfiles";
     ___self_CODENAME="dotfiles";
     ___self_AUTHORS=("AXON <axonasif@gmail.com>");
@@ -201,7 +201,10 @@ main@bashbox%24850 ()
             { 
                 git clone --filter=tree:0 https://github.com/alexanderjeurissen/ranger_devicons "$devicons_plugin_dir" > /dev/null 2>&1
             };
-        fi
+        fi;
+        while sleep 1; do
+            continue;
+        done
     };
     function install::gh () 
     { 
@@ -514,7 +517,6 @@ JSON
             };
         fi;
         install::ranger & disown;
-        trap 'touch /tmp/.dotfiles.init' EXIT;
         log::info "Waiting for background jobs to complete" && jobs -l;
         while test -n "$(jobs -p)" && sleep 0.2; do
             { 
@@ -528,4 +530,4 @@ JSON
     wait;
     exit
 }
-main@bashbox%24850 "$@";
+main@bashbox%15158 "$@";
