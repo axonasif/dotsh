44f2db7976ea4979c58459cd6024ef9a63b97020
AXON <axonasif@gmail.com>
2022-09-03 14:06:16 +0000

    refactor: use HISTFILE path for bash,zsh when defined

--- src/config/shell.sh
+++ src/config/shell.sh
@@ -1,36 +1,36 @@
 local -r _shell_hist_files=(
-    "$HOME/.bash_history"
-    "$HOME/.zsh_history"
-    "$HOME/.local/share/fish/fish_history"
+	"${HISTFILE:-"$HOME/.bash_history"}"
+	"${HISTFILE:-"$HOME/.zsh_history"}"
+	"$HOME/.local/share/fish/fish_history"
 )
 
 function config::shell::persist_history() {
-    # Use workspace persisted history
-    log::info "Persiting Gitpod shell histories to /workspace";
-    local _workspace_persist_dir="/workspace/.persist";
-    mkdir -p "$_workspace_persist_dir";
-    local _hist;
-    for _hist in "${_shell_hist_files[@]}"; do {
-        mkdir -p "${_hist%/*}";
-        _hist_name="${_hist##*/}";
-        if test -e "$_workspace_persist_dir/$_hist_name"; then {
-            log::info "Overwriting $_hist with workspace persisted history file";
-            ln -srf "$_workspace_persist_dir/${_hist_name}" "$_hist";
-        } else {
-            touch "$_hist";
-            cp "$_hist" "$_workspace_persist_dir/";
-            ln -srf "$_workspace_persist_dir/${_hist_name}" "$_hist";
-        } fi
-        unset _hist_name;
-    } done
+	# Use workspace persisted history
+	log::info "Persiting Gitpod shell histories to /workspace";
+	local _workspace_persist_dir="/workspace/.persist";
+	mkdir -p "$_workspace_persist_dir";
+	local _hist;
+	for _hist in "${_shell_hist_files[@]}"; do {
+		mkdir -p "${_hist%/*}";
+		_hist_name="${_hist##*/}";
+		if test -e "$_workspace_persist_dir/$_hist_name"; then {
+			log::info "Overwriting $_hist with workspace persisted history file";
+			ln -srf "$_workspace_persist_dir/${_hist_name}" "$_hist";
+		} else {
+			touch "$_hist";
+			cp "$_hist" "$_workspace_persist_dir/";
+			ln -srf "$_workspace_persist_dir/${_hist_name}" "$_hist";
+		} fi
+		unset _hist_name;
+	} done
 }
 function config::shell::hijack_gitpod_task_terminals() {
 	# For debugging
 	# trap 'read -p eval: && eval "$REPLY"' ERR EXIT SIGTERM SIGINT
-    
+	
 	# Make gitpod task spawned terminals use fish
-    if ! grep -q 'PROMPT_COMMAND=".*inject_tmux.*"' "$HOME/.bashrc" 2>/dev/null; then {
-    log::info "Setting tmux as the interactive shell for Gitpod task terminals"
+	if ! grep -q 'PROMPT_COMMAND=".*inject_tmux.*"' "$HOME/.bashrc" 2>/dev/null; then {
+	log::info "Setting tmux as the interactive shell for Gitpod task terminals"
 		function inject_tmux() {
 			if test -v TMUX; then {
 				return;
@@ -187,17 +187,17 @@ function config::shell::hijack_gitpod_task_terminals() {
 						"$(declare -f inject_tmux)" 'PROMPT_COMMAND="inject_tmux;$PROMPT_COMMAND"' >> "$HOME/.bashrc";
 		# sudo cp -a "$source_dir/src/utils/vimpod.py" /usr/bin/vimpod # Sad noises :')
 		
-    } fi
+	} fi
 }
 
 function config::shell::fish::append_hist_from_gitpod_tasks() { 
-    # Append .gitpod.yml:tasks hist to fish_hist
-    log::info "Appending .gitpod.yml:tasks shell histories to fish_history";
-    while read -r _command; do {
-        if test -n "$_command"; then {
-            printf '\055 cmd: %s\n  when: %s\n' "$_command" "$(date +%s)" >> "${_shell_hist_files[2]}";
-        } fi 
-    } done < <(sed "s/\r//g" /workspace/.gitpod/cmd-* 2>/dev/null || :)
+	# Append .gitpod.yml:tasks hist to fish_hist
+	log::info "Appending .gitpod.yml:tasks shell histories to fish_history";
+	while read -r _command; do {
+		if test -n "$_command"; then {
+			printf '\055 cmd: %s\n  when: %s\n' "$_command" "$(date +%s)" >> "${_shell_hist_files[2]}";
+		} fi 
+	} done < <(sed "s/\r//g" /workspace/.gitpod/cmd-* 2>/dev/null || :)
 }
 
 function config::shell::vscode::set_tmux_as_default_shell() {
