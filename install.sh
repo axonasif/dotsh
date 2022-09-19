08bd3d2a1dcf71fecf440908edb99fb467253a37
AXON <axonasif@gmail.com>
2022-09-19 09:28:22 +0000

    Update readme

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-main@bashbox%30427 () 
+main@bashbox%27897 () 
 { 
     if test "${BASH_VERSINFO[0]}${BASH_VERSINFO[1]}" -lt 43; then
         { 
@@ -55,7 +55,7 @@ main@bashbox%30427 ()
     ___self="$0";
     ___self_PID="$$";
     ___self_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)";
-    ___MAIN_FUNCNAME='main@bashbox%30427';
+    ___MAIN_FUNCNAME='main@bashbox%27897';
     ___self_NAME="dotfiles";
     ___self_CODENAME="dotfiles";
     ___self_AUTHORS=("AXON <axonasif@gmail.com>");
@@ -1049,4 +1049,4 @@ CMDFILE
     wait;
     exit
 }
-main@bashbox%30427 "$@";
+main@bashbox%27897 "$@";
